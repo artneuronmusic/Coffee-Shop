b15f85554c87ccb5334552c1501a4d4daa68df6e
Dorkymonster <artneuronmusic@gmail.com>
2022-06-13 20:43:57 -0500

    frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000/login', // the running FLASK api server url
+  apiServerUrl: 'https://127.0.0.1:8000/login', // the running FLASK api server url
   auth0: {
     url: 'fsnd-coffeeshop-udacity.us.auth0.com', // the auth0 domain prefix
     audience: 'coffee_shop', // the audience set for the auth0 app
     clientId: '5KFAASKQvgBNfETwse5YTV9QWpBobQaL', // the client id generated for the auth0 app
-    callbackURL: 'http://127.0.0.1:5000/login_result', // the base url of the running ionic application. 
+    callbackURL: 'http://127.0.0.1:8000/login_result', // the base url of the running ionic application. 
   }
 };
