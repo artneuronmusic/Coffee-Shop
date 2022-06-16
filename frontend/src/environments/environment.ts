f7fb30b01e181ab74c6480bec58f9b0aa1381d8e
vagrant <vagrant@vagrant.vm>
2022-06-16 06:49:20 +0000

    update frontend folder

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,15 @@
 /* @TODO replace with your variables
- * ensure all variables on this page match your project, http://localhost:8100 
+ * ensure all variables on this page match your project
  */
 
 export const environment = {
   production: false,
-  apiServerUrl: 'https://127.0.0.1:8000/login', // the running FLASK api server url
+  apiServerUrl: 'http://127.0.0.1:8000', // the running FLASK api server url
   auth0: {
     url: 'fsnd-coffeeshop-udacity.us', // the auth0 domain prefix
     audience: 'coffee_shop', // the audience set for the auth0 app
     clientId: '5KFAASKQvgBNfETwse5YTV9QWpBobQaL', // the client id generated for the auth0 app
-    callbackURL: 'http://127.0.0.1:8000/login_result', // the base url of the running ionic application. 
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
+
