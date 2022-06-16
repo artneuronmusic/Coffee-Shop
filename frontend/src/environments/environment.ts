86f52de7f64d41711b9424d590a8a502b24242b8
vagrant <vagrant@vagrant.vm>
2022-06-16 07:16:26 +0000

    delete postman tests and modify environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:8000', // the running FLASK api server url
+  apiServerUrl: 'http://127.0.0.1:8000/login', // the running FLASK api server url
   auth0: {
     url: 'fsnd-coffeeshop-udacity.us', // the auth0 domain prefix
     audience: 'coffee_shop', // the audience set for the auth0 app
     clientId: '5KFAASKQvgBNfETwse5YTV9QWpBobQaL', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'http://localhost:8100/', // the base url of the running ionic application. 
   }
 };
 
